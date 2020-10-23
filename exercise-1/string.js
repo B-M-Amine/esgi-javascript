82a976e1b043b97867dc6f82c29122815ab6fb60
Bouhadjar Mohamed El Amine <bouhadjarmedamine@gmail.com>
2020-10-23 14:37:11 +0200

    correctif

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -18,18 +18,19 @@ function prop_access(obj, path) {
         return obj;
     }
     if(obj == null){
-        return "";
+        console.log(path + " not exist");
+        return null;
     }
     let props = path.split('.');
     let result = obj;
     for (var i = 0; i < props.length; i++) {
         result = result[props[i]];
+        if (typeof result === 'undefined'){
+            console.log(path + " not exist");
+            return null;
+        }
     }
-    if (typeof result !== 'undefined'){
-            return result;
-    }else{
-        console.log(path + " not exist");
-    }
+        return result;
 }
 
 
